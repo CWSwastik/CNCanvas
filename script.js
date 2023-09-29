12f88ea5ac3d88dc76a603c43d1cdb40e876a296
Swas.py <codewithswastik@gmail.com>
2023-09-29 23:42:20 +0530

    Intial commit for v1.0

---
+++ script.js
@@ -0,0 +1,239 @@
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+const tools = document.getElementById("tools");
+const lineTool = document.getElementById("lineTool");
+const clockwiseTool = document.getElementById("clockwiseTool");
+const anticlockwiseTool = document.getElementById("anticlockwiseTool");
+const coordinatesSpan = document.getElementById("coordinates");
+const radiusSpan = document.getElementById("radius");
+
+let isDrawing = false;
+let startPoint = { x: 0, y: 0 };
+let endPoint = { x: 0, y: 0 };
+let curves = [];
+let curRadius = 50;
+
+function resizeCanvasToDisplaySize(canvas) {
+  const width = canvas.clientWidth;
+  const height = canvas.clientHeight;
+
+  if (canvas.width !== width || canvas.height !== height) {
+    canvas.width = width;
+    canvas.height = height;
+    return true;
+  }
+
+  return false;
+}
+
+resizeCanvasToDisplaySize(canvas);
+
+function drawLine(start, end) {
+  context.beginPath();
+  context.moveTo(start.x, start.y);
+  context.lineTo(end.x, end.y);
+  context.stroke();
+  context.closePath();
+}
+
+function calcCenterOfArc(start, end, radius, e) {
+  const d = Math.sqrt((end.x - start.x) ** 2 + (end.y - start.y) ** 2);
+  const u = (end.x - start.x) / d;
+  const v = (end.y - start.y) / d;
+  const h = Math.sqrt(radius ** 2 - d ** 2 / 4);
+  return {
+    x: (end.x + start.x) / 2 - e * h * v,
+    y: (end.y + start.y) / 2 + e * h * u,
+  };
+}
+
+function drawClockwiseCurve(start, end, radius) {
+  context.beginPath();
+
+  const center = calcCenterOfArc(start, end, radius, 1);
+
+  const startAngle = Math.atan2(start.y - center.y, start.x - center.x);
+  const endAngle = Math.atan2(end.y - center.y, end.x - center.x);
+
+  context.arc(center.x, center.y, radius, startAngle, endAngle, false);
+
+  context.stroke();
+  context.closePath();
+}
+
+function drawAnticlockwiseCurve(start, end, radius) {
+  context.beginPath();
+
+  const center = calcCenterOfArc(start, end, radius, -1);
+
+  const startAngle = Math.atan2(start.y - center.y, start.x - center.x);
+  const endAngle = Math.atan2(end.y - center.y, end.x - center.x);
+
+  context.arc(center.x, center.y, radius, startAngle, endAngle, true);
+
+  context.stroke();
+  context.closePath();
+}
+
+// Event listeners for tools
+let selectedTool = "line"; // Default to the straight line tool
+
+lineTool.addEventListener("click", () => {
+  selectedTool = "line";
+});
+
+clockwiseTool.addEventListener("click", () => {
+  selectedTool = "clockwise";
+});
+
+anticlockwiseTool.addEventListener("click", () => {
+  selectedTool = "anticlockwise";
+});
+
+function getCanvasPosition(event) {
+  const canvasRect = canvas.getBoundingClientRect();
+  return {
+    x: event.clientX - canvasRect.left,
+    y: event.clientY - canvasRect.top,
+  };
+}
+
+let isDrawingLine = false;
+
+canvas.addEventListener("mousedown", (e) => {
+  const canvasPos = getCanvasPosition(e);
+  if (selectedTool === "line") {
+    isDrawingLine = !isDrawingLine; // Toggle drawing state for straight lines
+    if (isDrawingLine) {
+      startPoint = { ...canvasPos };
+    } else {
+      endPoint = { ...canvasPos };
+      drawLine(startPoint, endPoint);
+      curves.push({
+        type: "line",
+        start: { ...startPoint },
+        end: { ...endPoint },
+      });
+    }
+  } else {
+    isDrawing = true;
+    startPoint = { ...canvasPos };
+    endPoint = { ...canvasPos };
+  }
+});
+
+function drawArcPreview() {
+  const d = Math.sqrt(
+    (endPoint.x - startPoint.x) ** 2 + (endPoint.y - startPoint.y) ** 2
+  );
+  if (curRadius < d / 2) {
+    curRadius = d / 2;
+  }
+
+  if (selectedTool === "clockwise") {
+    drawCurves();
+    drawClockwiseCurve(startPoint, endPoint, curRadius);
+  } else if (selectedTool === "anticlockwise") {
+    drawCurves();
+    drawAnticlockwiseCurve(startPoint, endPoint, curRadius);
+  }
+}
+canvas.addEventListener("mousemove", (e) => {
+  const canvasPos = getCanvasPosition(e);
+  endPoint = { ...canvasPos };
+  if (isDrawing && selectedTool !== "line") {
+    drawArcPreview();
+  } else if (isDrawingLine) {
+    drawCurves();
+    drawLine(startPoint, endPoint);
+  }
+
+  coordinatesSpan.textContent = `(${canvasPos.x.toFixed(
+    0
+  )}, ${canvasPos.y.toFixed(0)})`;
+  radiusSpan.textContent = `${curRadius.toFixed(2)}`;
+});
+
+// Function to draw all curves and lines in the curves array
+function drawCurves() {
+  context.clearRect(0, 0, canvas.width, canvas.height); // Clear the canvas
+  for (const curve of curves) {
+    if (curve.type === "line") {
+      drawLine(curve.start, curve.end);
+    } else if (curve.type === "clockwise") {
+      drawClockwiseCurve(curve.start, curve.end, curve.radius);
+    } else if (curve.type === "anticlockwise") {
+      drawAnticlockwiseCurve(curve.start, curve.end, curve.radius);
+    }
+  }
+}
+
+canvas.addEventListener("mouseup", (e) => {
+  if (isDrawing && selectedTool !== "line") {
+    isDrawing = false;
+    // Store the drawn curve in the curves array
+    const d = Math.sqrt(
+      (endPoint.x - startPoint.x) ** 2 + (endPoint.y - startPoint.y) ** 2
+    );
+    if (curRadius < d / 2) {
+      curRadius = d / 2;
+    }
+
+    curves.push({
+      type: selectedTool,
+      start: { ...startPoint },
+      end: { ...endPoint },
+      radius: curRadius,
+    });
+  } else if (isDrawingLine) {
+    // Finalize the straight line
+    isDrawingLine = false;
+    endPoint = { ...getCanvasPosition(e) };
+    drawLine(startPoint, endPoint);
+    curves.push({
+      type: "line",
+      start: { ...startPoint },
+      end: { ...endPoint },
+    });
+  }
+  drawCurves();
+  // console.log(curves);
+});
+
+canvas.addEventListener("wheel", (e) => {
+  if (selectedTool === "clockwise" || selectedTool === "anticlockwise") {
+    // If the circle tool is selected, adjust the radius based on the scroll direction
+    const delta = e.deltaY;
+    const radiusStep = 0.06 * curRadius;
+
+    if (delta > 0) {
+      // Scroll down to decrease the radius
+      curRadius -= radiusStep;
+    } else {
+      // Scroll up to increase the radius
+      curRadius += radiusStep;
+    }
+
+    // Ensure the radius is within a reasonable range
+
+    if (curRadius > 10000) {
+      curRadius = 10000;
+    }
+
+    // Update the coordinates box to display the adjusted radius
+    radiusSpan.textContent = `${curRadius.toFixed(2)}`;
+
+    // Redraw the circle with the adjusted radius
+    if (isDrawing) {
+      drawArcPreview();
+    }
+  }
+});
+
+const clearButton = document.getElementById("clearButton");
+
+clearButton.addEventListener("click", () => {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  curves.length = 0;
+});
